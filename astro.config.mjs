4667af7f15909146d99370faf8139bcba07f1540
Oksana T. <pochtoviy-yashik@mail.ru>
2025-06-02 11:01:24 +0400

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/telegram-bot' : '/',
+  site: process.env.NODE_ENV === 'production' ? 'https://psdhtmlcss.github.io' : 'http://localhost:4321'
+});
